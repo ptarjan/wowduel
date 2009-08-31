cd4a08cfb7a6f60b940f38d838569cc1123db350
Paul Tarjan <spam@paulisageek.com>
2009-08-31 10:44:33 -0700

    initial bare design for index page

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Wow Duel - Who will win?</title>
+</head>
+<body>
+    <h1>Wow Duel - Who will win?</h1>
+    <form action="fight.php">
+        <fieldset>
+            <legend>Pick 2 characters to fight</legend>
+            <table>
+            <tr><td>
+                Name
+            </td><td>
+                <input name="left_name" value="Vosk" />
+            </td><td>
+                <input name="right_name" value="Ariesz" />
+            </td></tr>
+            <tr><td>
+                Server
+            </td><td>
+                <input name="left_guild" value="Mal'Ganis" />
+            </td><td>
+                <input name="right_guild" value="Mal'Ganis" />
+            </td></tr>
+            <tr><td></td><td>
+                <input type="submit" value="Fight!" />
+            </td></tr>
+            </table>
+        </fieldset>
+    </form>
+</body>
+</html>
