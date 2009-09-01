f3ff13ba4a56125728312a29909f5e8d8c8e44ae
Paul Tarjan <spam@paulisageek.com>
2009-08-31 17:22:40 -0700

    turn the table the other way

--- index.php
+++ index.php
@@ -9,22 +9,22 @@
     <form action="fight.php">
         <fieldset>
             <legend>Pick 2 characters to fight</legend>
-            <table>
+            <table id="char_select">
+            <tr>
+                <th>Name</th>
+                <th>Server</th>
+            </tr>
             <tr><td>
-                Name
-            </td><td>
                 <input name="left_name" value="Vosk" />
+            </td><td>
+                <input name="left_server" value="Mal'Ganis" />
+            </td></tr>
+            <tr><td>
+                <input name="right_server" value="Mal'Ganis" />
             </td><td>
                 <input name="right_name" value="Ariesz" />
             </td></tr>
-            <tr><td>
-                Server
-            </td><td>
-                <input name="left_server" value="Mal'Ganis" />
-            </td><td>
-                <input name="right_server" value="Mal'Ganis" />
-            </td></tr>
-            <tr><td></td><td>
+            <tr><td colspan="2">
                 <input type="submit" value="Fight!" />
             </td></tr>
             </table>
