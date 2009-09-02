e63884cdc36d5003119eb241927848d7d03ac2cd
Paul Tarjan <spam@paulisageek.com>
2009-09-01 18:05:41 -0700

    clean out old data and i had the winning the wrong way

--- fight.js
+++ fight.js
@@ -9,23 +9,25 @@ Y.on("submit", fight, "form");
 function fight(e) {
     // Don't do the form submit
     e.halt();
+    Y.get("#result").setStyle("display", "hidden");
 
     var data = new Y.yql('use "http://paulisageek.com/wowduel/wowduel.xml" ; select * from wowduel where left_name="' + Y.get('[name="left_name"]').get("value") + '" AND left_server="' + Y.get('[name="left_server"]').get("value") + '" AND right_name="' + Y.get('[name="right_name"]').get("value") + '" AND right_server="' + Y.get('[name="right_server"]').get("value") + '"');
     data.on('query', function(r) {
         var result = r.results.result;
         var fight = result.left.ilevel - result.right.ilevel;
         if (fight < 0)
-            Y.get("#status").setContent(result.left.name + " Wins!");
-        else if (fight > 0)
             Y.get("#status").setContent(result.right.name + " Wins!");
+        else if (fight > 0)
+            Y.get("#status").setContent(result.left.name + " Wins!");
         else
             Y.get("#status").setContent("Tie!");
 
         function fill(root, data) {
             root.query("h1 a").setAttribute("href", data.url);
             root.query("h1 a").setContent(data.name);
-            root.query("p").append(data.ilevel);
+            root.query("p").setContent("Level: " + data.ilevel);
             var ul = root.query("ul");
+            ul.setContent(""); // clear anything out
             for each (var item in data.items) {
                 ul.append(
                     '<li><a href="http://www.wowhead.com/?item=' + item['id'] + '"><img src="http://www.wowarmory.com/wow-icons/_images/51x51/' + item['icon'] + '.jpg" /></a></li>'
