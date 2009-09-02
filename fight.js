6913be2639e4eba68002ef58dedff1c4c74f54e1
Paul Tarjan <spam@paulisageek.com>
2009-09-01 17:57:30 -0700

    do it for the left

--- fight.js
+++ fight.js
@@ -10,9 +10,29 @@ function fight(e) {
     // Don't do the form submit
     e.halt();
 
-    var data = new Y.yql('use "http://paulisageek.com/wowduel/wowduel.xml" ; select * from wowduel where left_name="Vosk" AND left_server="Mal\'Ganis" AND right_name="Ariesz" AND right_server="Mal\'Ganis"');
+    var data = new Y.yql('use "http://paulisageek.com/wowduel/wowduel.xml" ; select * from wowduel where left_name="' + Y.get('[name="left_name"]').get("value") + '" AND left_server="' + Y.get('[name="left_server"]').get("value") + '" AND right_name="' + Y.get('[name="right_name"]').get("value") + '" AND right_server="' + Y.get('[name="right_server"]').get("value") + '"');
     data.on('query', function(r) {
-        console.log(r.results);
+        var result = r.results.result;
+        console.log(result);
+        var fight = result.left.ilevel - result.right.ilevel;
+        if (fight < 0)
+            Y.get("#status").setContent(result.left.name + " Wins!");
+        else if (fight > 0)
+            Y.get("#status").setContent(result.right.name + " Wins!");
+        else
+            Y.get("#status").setContent("Tie!");
+
+        Y.get("#left h1 a").setAttribute("href", result.left.url);
+        Y.get("#left h1 a").setContent(result.left.name);
+        Y.get("#left p").append(result.left.ilevel);
+        var ul = Y.get("#left ul");
+        for each (var item in result.left.items) {
+            console.log(item);
+            ul.append(
+                '<li><a href="http://www.wowhead.com/?item=' + item['id'] + '"><img src="http://www.wowarmory.com/wow-icons/_images/51x51/' + item['icon'] + '.jpg" /></a></li>'
+            );
+        }
+        Y.get("#result").setStyle("display", "block");
     });
 }
 });
