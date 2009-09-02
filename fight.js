f31ba747f627a1c4d66f47c0f739538d71006d43
Paul Tarjan <spam@paulisageek.com>
2009-09-01 18:03:30 -0700

    and factor it to both sides

--- fight.js
+++ fight.js
@@ -13,7 +13,6 @@ function fight(e) {
     var data = new Y.yql('use "http://paulisageek.com/wowduel/wowduel.xml" ; select * from wowduel where left_name="' + Y.get('[name="left_name"]').get("value") + '" AND left_server="' + Y.get('[name="left_server"]').get("value") + '" AND right_name="' + Y.get('[name="right_name"]').get("value") + '" AND right_server="' + Y.get('[name="right_server"]').get("value") + '"');
     data.on('query', function(r) {
         var result = r.results.result;
-        console.log(result);
         var fight = result.left.ilevel - result.right.ilevel;
         if (fight < 0)
             Y.get("#status").setContent(result.left.name + " Wins!");
@@ -22,16 +21,21 @@ function fight(e) {
         else
             Y.get("#status").setContent("Tie!");
 
-        Y.get("#left h1 a").setAttribute("href", result.left.url);
-        Y.get("#left h1 a").setContent(result.left.name);
-        Y.get("#left p").append(result.left.ilevel);
-        var ul = Y.get("#left ul");
-        for each (var item in result.left.items) {
-            console.log(item);
-            ul.append(
-                '<li><a href="http://www.wowhead.com/?item=' + item['id'] + '"><img src="http://www.wowarmory.com/wow-icons/_images/51x51/' + item['icon'] + '.jpg" /></a></li>'
-            );
+        function fill(root, data) {
+            root.query("h1 a").setAttribute("href", data.url);
+            root.query("h1 a").setContent(data.name);
+            root.query("p").append(data.ilevel);
+            var ul = root.query("ul");
+            for each (var item in data.items) {
+                ul.append(
+                    '<li><a href="http://www.wowhead.com/?item=' + item['id'] + '"><img src="http://www.wowarmory.com/wow-icons/_images/51x51/' + item['icon'] + '.jpg" /></a></li>'
+                );
+            }
         }
+
+        fill(Y.get("#left"), result.left);
+        fill(Y.get("#right"), result.right);
+
         Y.get("#result").setStyle("display", "block");
     });
 }
